f99315df921de334875685adad627ac8f040f451
Akhilesh Nirna <30986640+HighOnWater@users.noreply.github.com>
2018-03-22 22:57:40 +0530

    Add files via upload

---
+++ xv6Imp/date.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main(int argc, char *argv[])
+{
+  struct rtcdate r;
+
+  if (date(&r)) {
+    printf(2, "date failed\n");
+    exit();
+  }
+
+   printf(1,"%d %d %d\n", r.day,r.month,r.year);  
+
+  exit();
+}
+
